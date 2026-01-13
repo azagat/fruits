77aa3abba5a5a80bfc78d0db75f693fc0909af61
Amorie Zsako <az123@wellesley.edu>
2026-01-13 10:42:35 -0500

    Added user input, fav fruit check

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ fruits = ("apple", "banana", "oranges", "dragon fruit")
 
 for fruit in fruits:
     print(fruit)
+    
+
+userFruit = input("What is your favorite fruit? ")
+
+if userFruit in fruits:
+    print("Nice, mine too!")
+else:
+    print("Not my favorite, but good choice!")
