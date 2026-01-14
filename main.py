fcd2e0c344ca7d42649c2c8e16bb99c9d1e5c2d6
Amorie Zsako <az123@wellesley.edu>
2026-01-14 08:34:04 -0500

    Added smiley at end of question

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ fruits = ("apple", "banana", "oranges", "dragon fruit")
 
 for fruit in fruits:
     print(fruit)
-    
 
 userFruit = input("What is your favorite fruit? ")
+userFruit = input(":D")
 
 if userFruit in fruits:
     print("Nice, mine too!")
